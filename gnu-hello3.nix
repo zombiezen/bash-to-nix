bab05f88fa91550650c65339e2350cda2c4937f0
Ross Light <ross@zombiezen.com>
2023-03-02 11:20:59 -0800

    Add explicit phases

--- gnu-hello3.nix
+++ gnu-hello3.nix
@@ -7,8 +7,4 @@ pkgs.stdenv.mkDerivation {
     url = "mirror://gnu/hello/hello-2.12.1.tar.gz";
     hash = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
   };
-
-  configurePhase = "./configure --prefix=$out";
-  buildPhase = "make";
-  installPhase = "make install";
 }
